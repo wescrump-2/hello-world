8ebe821263f23227f521420e7d7310fd86894c53
Wes Crump <wes.crump@gmail.com>
2025-01-07 12:15:20 -0600

    removed old commented out code

--- src/util.ts
+++ src/util.ts
@@ -45,36 +45,3 @@ export class Util {
         return `${hexUUID.slice(0, 8)}-${hexUUID.slice(8, 12)}-${hexUUID.slice(12, 16)}-${hexUUID.slice(16, 20)}-${hexUUID.slice(20)}`;
     }
 }
-
-
-
-
-// export class Util {
-// 	static ID = "com.wescrump.initiative-tracker"
-// 	static PlayerMkey = `${Util.ID}/player`
-// 	static DeckMkey = `${Util.ID}/deck`
-	
-// 	static rem2px(remstr: string): number {
-// 		const rem = parseFloat(remstr)
-// 		return rem * parseFloat(getComputedStyle(document.documentElement).fontSize)
-// 	}
-
-// 	static shortUUID(): string {
-// 		const uuid = crypto.randomUUID()
-// 		const cleanUUID = uuid.replace(/-/g, '');
-// 		const byteArray = new Uint8Array(cleanUUID.match(/.{1,2}/g)!.map(byte => parseInt(byte, 16)));
-// 		const base64UUID = btoa(String.fromCharCode(...byteArray));
-// 		return base64UUID.replace(/=+$/, '').replace(/\+/g, '-').replace(/\//g, '_');
-// 	}
-
-// 	static expandUUID(shortUUID: string): string {
-// 		let paddedUUID = shortUUID + '=='.slice(0, (4 - shortUUID.length % 4) % 4);
-// 		const byteString = atob(paddedUUID.replace(/-/g, '+').replace(/_/g, '/'));
-// 		const byteArray = new Uint8Array(byteString.length);
-// 		for (let i = 0; i < byteString.length; i++) {
-// 			byteArray[i] = byteString.charCodeAt(i);
-// 		}
-// 		const hexUUID = Array.from(byteArray, byte => byte.toString(16).padStart(2, '0')).join('');
-// 		return `${hexUUID.slice(0, 8)}-${hexUUID.slice(8, 12)}-${hexUUID.slice(12, 16)}-${hexUUID.slice(16, 20)}-${hexUUID.slice(20)}`;
-// 	}
-// }
